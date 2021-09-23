cd1710887e5d629227a9e8bdf52728cb00ae609a
Hertz <kljj0650@naver.com>
2021-09-23 20:07:36 +0900

    문제 풀이

---
+++ LEVEL1/나누어 떨어지는 숫자 배열.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+
+    vector<int> answer;
+
+    for (int i=0; i<arr.size(); ++i) {
+        if (arr[i] % divisor == 0) {
+            answer.push_back(arr[i]);
+        }
+    }
+
+    if (answer.size() == 0) {
+
+        answer.push_back(-1);
+
+    } else {
+        
+        sort(answer.begin(), answer.end());
+    }
+
+    return answer;
+}
+
+int main(void) {
+
+    vector<int> arr = {5, 9, 7, 10};
+    int divisor = 5;
+
+    vector<int> result = solution(arr, divisor);
+
+    for (int i=0; i<result.size(); ++i) {
+        cout << result[i] << " ";
+    } cout << endl;
+
+    return 0;
+}
