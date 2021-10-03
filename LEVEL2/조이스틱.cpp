fee5f3675a858b29fd370cd8bf8812d9ce33126b
Hertz <kljj0650@naver.com>
2021-10-03 10:32:58 +0900

    문제 풀이

---
+++ LEVEL2/조이스틱.cpp
@@ -0,0 +1,78 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int solution(string name) {
+
+    int answer = 0;
+
+    for (int i=0; i<name.size(); ++i) {
+        int num1 = name[i] - 'A';
+        int num2 = 'Z' + 1 - name[i];
+        int num3 = (num1 < num2) ? num1 : num2;
+        answer += num3;
+    }
+
+    // 1. 왼쪽으로 탐색
+    int move_left = 0;
+    int temp = 0;
+    for (int i=name.size()-1; i>=1; --i) {
+        if (name[i] == 'A') {
+            ++temp;
+        } else {
+            move_left += temp + 1;
+            temp = 0;
+        }
+    }
+
+    // 2. 오른쪽으로 탐색
+    int move_right = 0;
+    temp = 0;
+    for (int i=1; i<name.size(); ++i) {
+        if (name[i] == 'A') {
+            ++temp;
+        } else {
+            move_right += temp + 1;
+            temp = 0;
+        }
+    }
+
+    name[0] = 'A';
+    // 3. 오른쪽으로 가다가 왼쪽으로 탐색
+    int move_right_and_left = 999;
+    // 꺾는 위치
+    for (int i=1; i<name.size(); ++i) {
+        int move = 0;
+        move = i + i;
+        temp = 0;
+        for (int j=name.size()-1; j>i; --j) {
+            if (name[j] == 'A') {
+                ++temp;
+            } else {
+                move += temp + 1;
+                temp = 0;
+            }
+        }
+        if (move_right_and_left > move) move_right_and_left = move;
+    }
+
+    int min = move_left;
+    if (min > move_right) min = move_right;
+    if (min > move_right_and_left) min = move_right_and_left;
+
+    answer += min;
+
+    return answer;
+}
+
+int main(void) {
+
+    string name = "ZAAAZZZZZZZ";
+    int result = solution(name);
+  
+    cout << result << endl;
+ 
+    return 0;
+}
