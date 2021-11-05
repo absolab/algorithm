2c5b61a565ea846b6fcc2fecda9b80a150aa9922
Hertz <kljj0650@naver.com>
2021-11-05 23:07:33 +0900

    문제 풀이 (힌트 봄)

---
+++ LEVEL2/가장 큰 정사각형 찾기.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int dp[1001][1001];
+
+int solution(vector<vector<int>> board) {
+
+    int answer = 0;
+
+    int row = board.size();
+    int col = board[0].size();
+
+    answer = board[0][0];
+
+    for (int i=1; i<row; ++i) {
+        for (int j=1; j<col; ++j) {
+            if (board[i][j] == 0) continue;
+
+            int n1 = board[i-1][j-1];
+            int n2 = board[i][j-1];
+            int n3 = board[i-1][j];
+
+            board[i][j] = min(n1, min(n2, n3)) + 1;
+
+            answer = max(answer, board[i][j]);
+        }
+    }
+
+    return answer*answer;
+}
+
+int main(void) {
+
+    vector<vector<int>> board = {{0,1,1,1},{1,1,1,1},{1,1,1,1},{0,0,1,0}};;
+
+    int result = solution(board);
+
+    cout << result << endl;
+
+    return 0;
+}
