25ec57d0048bb0c82536865aa5b761c514fe8914
Hertz <kljj0650@naver.com>
2021-12-09 08:43:02 +0900

    업로드용 코드 정리 및 폴더 이동

--- 블로그에 올림/Lv2/124 나라의 숫자.cpp
+++ 블로그에 올림/Lv2/124 나라의 숫자.cpp
@@ -8,8 +8,9 @@ string solution(int n) {
 
     string answer = "";
     int triad[19];
-    // 3^18 = 387,420,489
-    int div = 387420489;
+
+    // 3진수로 변환
+    int div = 387420489;    // 3^18 = 387,420,489
     bool is_first_num = true;
     int first_num_pos = 0;
     for (int i=0; i<19; ++i) {
@@ -25,13 +26,16 @@ string solution(int n) {
         div /= 3;
     }
 
+    // 124 진수로 변환
     for (int i=18; i>first_num_pos; --i) {
-        if (triad[i] == 0 && i != 0) {
-            
+
+        // 0일 경우 앞에서 숫자를 당긴 후 4로 변환한다.
+        if (triad[i] == 0 && i != 0) {            
             triad[i-1]--;
             triad[i] = 4;
+        
+        // 이전 자릿수에서 1을 당겨오는 바람에 -1이 된 경우엔 앞에서 당긴 후 2로 변환한다
         } else if (triad[i] == -1 && i != 0) {
-
             triad[i-1]--;
             triad[i] = 2;
         } 
--- 블로그에 올림/Lv2/멀쩡한 사각형.cpp
+++ 블로그에 올림/Lv2/멀쩡한 사각형.cpp
@@ -8,27 +8,26 @@ long long solution(int w,int h) {
 
     long long answer = 1;
 
-    if (w > h) {
-        int temp = w;
-        w = h;
-        h = temp;
-    }
+    // 가로가 짧고 세로가 길도록 설정 (Swap)
+    if (w > h) { int temp = w; w = h; h = temp; }
 
+    // 유클리드 호제법을 이용하여 최소공배수를 구함
     int gcd = w;
     int temp = h;
-
     while(1) {
 
         int r = temp % gcd;
-        if (r == 0) break; 
+        if (r == 0) { break; }
 
         temp = gcd;
         gcd = r;        
     }
 
+    // 작은 사각형으로 만들기
     int small_w = w / gcd;
     int small_h = h / gcd;
 
+    // 대각선이 지나가는 사각형의 개수 구하기
     int next_y = 1;
     int count = 0;    
 
@@ -38,21 +37,17 @@ long long solution(int w,int h) {
         long long y_r = ((long long)x*small_w) % small_h;
 
         if (y_r == 0) {
-
             ++count;
             ++next_y;
-
         } else if (y - next_y < 0) {
-
             ++count;
-
         } else { // y-next_y > 0
             count += 2;
             ++next_y;
         }
     }
-
-    answer = ((long long)w * h) - (long long)gcd * count;
+    
+    answer = ((long long)w * h) - (long long)count * gcd;
 
     return answer;
 }
