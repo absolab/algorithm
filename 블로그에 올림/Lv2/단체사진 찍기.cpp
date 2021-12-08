7574f0d8d9d149a008afcaa2b0e4d5aef2c92f80
Hertz <kljj0650@naver.com>
2021-12-08 11:50:37 +0900

    업로드를 위한 코드 수정 및 이동

--- 블로그에 올림/Lv2/단체사진 찍기.cpp
+++ 블로그에 올림/Lv2/단체사진 찍기.cpp
@@ -3,7 +3,6 @@
 #include <iostream>
 
 #include <algorithm>
-#include <map>
 using namespace std;
 
 int solution(int n, vector<string> data) {
@@ -11,36 +10,29 @@ int solution(int n, vector<string> data) {
     int answer = 0;
 
     bool is_visited[8] = {0, };
-    vector<int> line = {0, 1, 2, 3, 4, 5, 6, 7};
-    map<char, int> char_to_int;
-
-    char_to_int.insert(make_pair('A', 0));
-    char_to_int.insert(make_pair('C', 1));
-    char_to_int.insert(make_pair('F', 2));
-    char_to_int.insert(make_pair('J', 3));
-    char_to_int.insert(make_pair('M', 4));
-    char_to_int.insert(make_pair('N', 5));
-    char_to_int.insert(make_pair('R', 6));
-    char_to_int.insert(make_pair('T', 7));
+    vector<char> line = {'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
     
     do {
         bool is_satisfied = true;
 
         for (int i=0; i<n; ++i) {
             
-            int friend1 = char_to_int[data[i][0]];
-            int friend2 = char_to_int[data[i][2]];
+            char friend1 = data[i][0];
+            char friend2 = data[i][2];
             int friend1_pos, friend2_pos;
 
+            // 위치 확인
             for (int j=0; j<8; ++j) {
                 if (line[j] == friend1) friend1_pos = j;
                 if (line[j] == friend2) friend2_pos = j;
             }
 
-            int gap = abs(friend1_pos - friend2_pos);
-            int want_gap = data[i][4] - '0' + 1;
+            // 조건 계산
+            int gap = abs(friend1_pos - friend2_pos);   // 현재 간격
+            int want_gap = data[i][4] - '0' + 1;        // 원하는 간격
             char c = data[i][3];
 
+            // 조건 확인
             if (c == '=' && gap != want_gap) {
                 is_satisfied = false;
             } else if (c == '>' && gap <= want_gap) {
@@ -50,9 +42,8 @@ int solution(int n, vector<string> data) {
             }
         }
 
-        if (is_satisfied) {
-            ++answer;
-        }
+        if (is_satisfied) { ++answer; }     // 조건을 만족하면 1 추가
+
     } while(next_permutation(line.begin(), line.end()));
 
     return answer;
