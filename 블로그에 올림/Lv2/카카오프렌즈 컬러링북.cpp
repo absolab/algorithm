9ddf78bd4749a1dccbcda325f26c6a339e62b4ee
Hertz <kljj0650@naver.com>
2021-12-08 11:17:46 +0900

    주석 추가 및 파일 이동

--- 블로그에 올림/Lv2/카카오프렌즈 컬러링북.cpp
+++ 블로그에 올림/Lv2/카카오프렌즈 컬러링북.cpp
@@ -6,27 +6,29 @@ using namespace std;
 
 vector<int> solution(int m, int n, vector<vector<int>> picture) {
 
-    int number_of_area = 0;
-    int max_size_of_one_area = 0;
+    int number_of_area = 0;         // 영역의 개수
+    int max_size_of_area = 0;       // 가장 큰 영역의 크기
 
     int dx[] = {0, 0, 1, -1};
     int dy[] = {1, -1, 0, 0};
 
-    bool is_colored[100][100] = {false, };
+    bool is_colored[100][100] = {false, };  // 방문 했었는지 확인
 
     for (int i=0; i<m; ++i) {
 
         for (int j=0; j<n; ++j) {
 
             int size_of_area = 0;
-            vector<pair<int,int>> same_color;
             const int current_color = picture[i][j];
 
-            if (is_colored[i][j]) { continue; }
-            if (picture[i][j] == 0) { continue; }
+            if (is_colored[i][j]) { continue; }     // 이미 방문 했으면 패스
+            if (picture[i][j] == 0) { continue; }   // 색깔이 없으면 패스
 
-            same_color.push_back(make_pair(i, j));
             ++number_of_area;
+
+            // DFS
+            vector<pair<int,int>> same_color;       // 같은 영역의 좌표 값 저장
+            same_color.push_back(make_pair(i, j));  
 
             while(same_color.size() > 0) {
 
@@ -36,19 +38,20 @@ vector<int> solution(int m, int n, vector<vector<int>> picture) {
                 int pre_i = pre_pos.first;
                 int pre_j = pre_pos.second;
 
-                if (is_colored[pre_i][pre_j]) continue;
+                if (is_colored[pre_i][pre_j]) continue;     // 이미 방문 했으면 패스
 
-                is_colored[pre_i][pre_j] = true;
+                is_colored[pre_i][pre_j] = true;            // 방문 한걸로 변경
                 ++size_of_area;
 
+                // 상하좌우 확인
                 for (int k=0; k<4; ++k) {
 
                     int new_i = pre_i+dx[k];
                     int new_j = pre_j+dy[k];
 
-                    if (new_i < 0 || new_i >= m) { continue; }
-                    if (new_j < 0 || new_j >= n) { continue; }
-                    if (is_colored[new_i][new_j]) { continue; }
+                    if (new_i < 0 || new_i >= m) { continue; }  // 범위를 벗어남
+                    if (new_j < 0 || new_j >= n) { continue; }  // 범위를 벗어남
+                    if (is_colored[new_i][new_j]) { continue; } // 이미 방문함
 
                     if (picture[new_i][new_j] == current_color) {
                         same_color.push_back(make_pair(new_i, new_j));
@@ -56,16 +59,17 @@ vector<int> solution(int m, int n, vector<vector<int>> picture) {
 
                 }
             }
-        
-            if (max_size_of_one_area < size_of_area) {
-                max_size_of_one_area = size_of_area;
+
+            // 가장 큰 영역의 크기 저장
+            if (max_size_of_area < size_of_area) {
+                max_size_of_area = size_of_area;
             }
         }
     }
 
     vector<int> answer(2);
     answer[0] = number_of_area;
-    answer[1] = max_size_of_one_area;
+    answer[1] = max_size_of_area;
     return answer;
 }
 
