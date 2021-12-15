6fcf4d798f8a28bec16bd38c09aa6f17a15ab7ef
Hertz <kljj0650@naver.com>
2021-12-15 09:33:42 +0900

    업로드를 위한 주석문 작성 및 폴더 이동

--- 블로그에 올림/Lv2/짝지어 제거하기.cpp
+++ 블로그에 올림/Lv2/짝지어 제거하기.cpp
@@ -4,25 +4,36 @@
 
 using namespace std;
 
-char cs[1000000];
-int idx = 0;
+char cs[1000000];       // 스택
+int idx = 0;            // top의 인덱스
 
 int solution(string s) {
 
     int answer = 0;
 
+    // 초기값 지정
     cs[0] = s[0];
 
     for (int i=1; i<s.size(); ++i) {
+
+        // 스택이 비어있으면 push
         if (idx == -1) {
+
             cs[++idx] = s[i];
+
+        // 스택에 들어있는 값과 같으면 pop
         } else if (cs[idx] == s[i]) {
+
             --idx;
+
+        // 스택에 들어있는 값과 다르면 push
         } else {
+
             cs[++idx] = s[i];
         }
     }
 
+    // 스택이 비어있으면 1 반환
     if (idx == -1) answer = 1;
 
     return answer;
