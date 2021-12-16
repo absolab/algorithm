b03c79438c9fb818f0b461d67a31c0bd5c1fcee0
Hertz <kljj0650@naver.com>
2021-12-16 10:30:33 +0900

    업로드를 위한 주석문 작성 및 폴더 이동

--- 블로그에 올림/Lv2/행렬 테두리 회전하기.cpp
+++ 블로그에 올림/Lv2/행렬 테두리 회전하기.cpp
@@ -7,9 +7,10 @@ using namespace std;
 vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
 
     vector<int> answer;
-    vector<vector<int>> arr;
+    vector<vector<int>> arr;    // 회전 시킬 배열
     int value = 0;
 
+    // 배열 초기화 
     for (int i=0; i<rows; ++i) {
         arr.push_back(vector<int>());
         for (int j=0; j<columns; ++j) {
@@ -19,13 +20,19 @@ vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
 
     for (int i=0; i<queries.size(); ++i) {
 
+        // LT
         int x1 = queries[i][0];
         int y1 = queries[i][1];
+        // RB
         int x2 = queries[i][2];
         int y2 = queries[i][3];
         
-        int min = 123456;
+        int min = 123456;   // 최대 100x100 행렬
 
+        // 회전하기
+        // 중간에 움직이는 숫자의 최솟값을 업데이트 한다
+
+        // 윗줄 오른쪽으로 밀기
         int next_value = arr[x1-1][y1-1];
         if (next_value < min) min = next_value;
         int temp;
@@ -36,6 +43,7 @@ vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
             if (next_value < min) min = next_value;
         }
 
+        // 오른쪽줄 아래로 밀기
         for (int j=1; j<=x2-x1; ++j) {
             temp = next_value;
             next_value = arr[x1-1+j][y2-1];
@@ -43,6 +51,7 @@ vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
             if (next_value < min) min = next_value;
         }
 
+        // 아랫줄 왼쪽으로 밀기
         for (int j=1; j<= y2-y1; ++j) {
             temp = next_value;
             next_value = arr[x2-1][y2-1-j];
@@ -50,6 +59,7 @@ vector<int> solution(int rows, int columns, vector<vector<int>> queries) {
             if (next_value < min) min = next_value;
         }
 
+        // 왼쪽줄 위로 밀기
         for (int j=1; j<= x2-x1; ++j) {
             temp = next_value;
             next_value = arr[x2-1-j][y1-1];
