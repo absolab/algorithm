91c68bd811f3e50a19ee8e6c3ac4360999242d73
Hertz <kljj0650@naver.com>
2021-12-29 04:19:51 +0900

    블로그 업로드를 위한 주석문 작성

--- LEVEL2/프린터.cpp
+++ LEVEL2/프린터.cpp
@@ -7,14 +7,22 @@ using namespace std;
 int solution(vector<int> priorities, int location) {
 
     int answer = 0;
-    int priorities_count[10] = {0, };
+    int priorities_count[10] = {0, };       // 1~9 중요도 문서의 개수
     int max = -1;
 
-    vector<pair<int,bool>> my_priorities;
+    vector<pair<int,bool>> my_priorities;   // 뽑아야 하는 문서를 표시할 수 있는 컨테이너
+
     for (int i=0; i<priorities.size(); ++i) {
-        if (max < priorities[i]) {max = priorities[i];}
+
+        // 중요도 최대값 갱신
+        if (max < priorities[i]) {
+            max = priorities[i];
+        }
+
+        // 해당 중요도 문서 개수 추가
         ++priorities_count[priorities[i]];
 
+        // 복사 배열에 값 추가 (뽑아야 하는 문서 표시)
         if (i == location) {
             my_priorities.push_back(make_pair(priorities[i], true));
         } else {
@@ -25,6 +33,7 @@ int solution(vector<int> priorities, int location) {
 
     for (int i=0; i<my_priorities.size(); ++i) {
 
+        // 최대 중요도 문서를 다 뽑았으면 한단계 밑으로
         if (priorities_count[max] == 0) {
             max--;
             i--;
@@ -34,10 +43,14 @@ int solution(vector<int> priorities, int location) {
         if (my_priorities[i].first == max) {
             ++answer;
             --priorities_count[max];
+
+            // 뽑아야 하는 문서면 끝
             if (my_priorities[i].second == true) {
                 break;
             }
+
         } else {
+            // 계속 뒤에 추가
             my_priorities.push_back(my_priorities[i]);
         }
     }
