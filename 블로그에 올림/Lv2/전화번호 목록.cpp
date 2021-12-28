5f5c9e72c32ab83749e797e3c2367fdf24b9f16d
Hertz <kljj0650@naver.com>
2021-12-28 09:46:48 +0900

    업로드를 위한 주석문 작성 및 폴더 이동

--- 블로그에 올림/Lv2/전화번호 목록.cpp
+++ 블로그에 올림/Lv2/전화번호 목록.cpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 using namespace std;
 
+// sort의 비교 함수
 bool compare(string i, string j) { return i.size() > j.size(); }
 
 bool solution(vector<string> phone_book) {
@@ -13,11 +14,13 @@ bool solution(vector<string> phone_book) {
     bool answer = true;
     map<string , bool> check;
 
+    // 길이가 긴 순으로 정렬
     sort(phone_book.begin(), phone_book.end(), compare);
 
     for (int i=0; i<phone_book.size(); ++i) {
         string s = phone_book[i];
         if (check.find(s) == check.end()) {
+            // 접두사가 될 수 있는 문자열을 전부 등록
             for (int i=1; i<=s.size(); ++i) {
                 check.insert(make_pair(s.substr(0, i), true));
             }
