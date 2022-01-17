8957b068b51fc8b1b091308994d28f9f1c8e4dfc
Hertz <kljj0650@naver.com>
2022-01-17 09:54:20 +0900

    업로드를 위한 주석문 작성 및 폴더 이동

--- 블로그에 올림/Lv2/다리를 지나는 트럭.cpp
+++ 블로그에 올림/Lv2/다리를 지나는 트럭.cpp
@@ -12,18 +12,24 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int left = 0;
     int total_weight = 0;
 
+    // 다리 위에 있는 트럭과 여태까지 걸린 시간
     queue<pair<int,int>> q;
 
     do {
 
         if (q.size() != 0) {
+            
+            // 다리를 건넌 트럭이 있는지 확인
             if (answer - q.front().second == bridge_length) {
                 int temp = q.front().first;
                 q.pop();
                 total_weight -= temp;
             }
         }
+
+        // 남아있는 트럭이 있으면
         if (left < truck_weights.size()) {
+            // 다리가 견딜 수 있는 무게를 초과하지 않으면
             if (total_weight + truck_weights[left] <= weight) {
                 total_weight += truck_weights[left];
                 q.push(make_pair(truck_weights[left], answer));
