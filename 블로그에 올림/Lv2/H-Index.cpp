6bc66ecddc8aa19430def7db633e4adab7551d14
Hertz <kljj0650@naver.com>
2022-01-24 15:48:46 +0900

    업로드를 위한 주석문 작성 및 폴더 이동

--- 블로그에 올림/Lv2/H-Index.cpp
+++ 블로그에 올림/Lv2/H-Index.cpp
@@ -8,10 +8,14 @@ int solution(vector<int> citations) {
 
     int answer = 0;
 
+    // 인용된 순으로 오름차순 정렬
     sort(citations.begin(), citations.end());
 
     for (int i=0; i<citations.size(); ++i) {
+
+        // H index 조건 확인 (큰 값부터)
         if (citations.size() - i <= citations[i]) { 
+
             answer = citations.size() - i;
             break;
         }
