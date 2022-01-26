05979f4ee5758bd69308ce29099ceefbf3c31797
Hertz <kljj0650@naver.com>
2022-01-26 09:12:46 +0900

    업로드를 위한 주석문 작성 및 폴더 이동

--- 블로그에 올림/Lv2/카펫.cpp
+++ 블로그에 올림/Lv2/카펫.cpp
@@ -8,12 +8,15 @@ vector<int> solution(int brown, int yellow) {
 
     vector<int> answer;
 
+    // 너비 + 높이 (테두리 / 2 + 반으로 잘린 놈 2개)
     int width_heigth = brown / 2 + 2;
 
+    // 최소 너비는 3
     for (int i=3; i<width_heigth; ++i) {
         int heigth = i;
         int width = width_heigth - i;
 
+        // 테두리를 제외한 너비 == 노란색
         if ( (heigth-2) * (width-2) == yellow) {
             answer.push_back(width);
             answer.push_back(heigth);
