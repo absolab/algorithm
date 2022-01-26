c5983310cc054f576ab84c0029a3bf10546b4dba
Hertz <kljj0650@naver.com>
2022-01-26 10:22:14 +0900

    업로드를 위한 주석문 작성 및 폴더 이동

--- 블로그에 올림/Lv2/큰 수 만들기.cpp
+++ 블로그에 올림/Lv2/큰 수 만들기.cpp
@@ -10,6 +10,7 @@ string solution(string number, int k) {
 
     vector<int> v;
 
+    // 입력받은 숫자를 각 각 벡터에 담는다
     for (int i=0; i<number.size(); ++i) {
         v.push_back(number[i] - '0');
     }
@@ -18,11 +19,13 @@ string solution(string number, int k) {
     int erase_count = 0;
     int idx = 0;
     while (erase_count != k) {
+        // 맨 처음
         if (v2.size() == 0) {
             v2.push_back(v[idx++]);
             continue;
         }
 
+        // 뒤에 더 큰 숫자가 있으면 앞에 숫자를 지운다
         if (v2.back() < v[idx]) {
             v2.pop_back();
             erase_count++;
