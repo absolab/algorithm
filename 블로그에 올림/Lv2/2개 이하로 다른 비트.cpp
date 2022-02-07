4a69cfde4f9955b57186c361cc7dd1e91d49ff03
Hertz <kljj0650@naver.com>
2022-02-07 09:17:28 +0900

    업로드를 위한 주석문 작성 및 폴더 이동

--- 블로그에 올림/Lv2/2개 이하로 다른 비트.cpp
+++ 블로그에 올림/Lv2/2개 이하로 다른 비트.cpp
@@ -11,19 +11,31 @@ vector<long long> solution(vector<long long> numbers) {
     for (int i=0; i<numbers.size(); ++i) {
 
         long long num = numbers[i];
+
+        // 오른쪽 비트 2개가 11일 경우
         if (num % 4 == 3) {
+
             long long copy_num = num;
             long long d = 1;
+
+            // 01을 찾아 10으로 변경한다
             while (1) {
-                if (copy_num % 4 == 1) {
+
+                if (copy_num % 4 == 1) {    
+
                     answer.push_back(num + d);
                     break;
+
                 } else {
+
                     copy_num /= 2;
                     d *= 2;
                 }
             }
+
+        // 오른쪽 비트 2개가 00, 01, 10인 경우
         } else {
+
             answer.push_back(num + 1);
         }
     }
