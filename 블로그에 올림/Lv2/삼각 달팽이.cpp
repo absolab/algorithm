85bf3836532bd448421a1213ceac3fb9fe7a06dc
Hertz <kljj0650@naver.com>
2022-02-08 14:13:10 +0900

    주석문 작성 및 폴더 이동

--- 블로그에 올림/Lv2/삼각 달팽이.cpp
+++ 블로그에 올림/Lv2/삼각 달팽이.cpp
@@ -4,28 +4,30 @@
 
 using namespace std;
 
-int arr[1000][1000];
+// n은 1 이상 1000 이하
+int arr[1001][1001];
 
 vector<int> solution(int n) {
 
     vector<int> answer;
 
-    int cnt = 1;
-    for (int i=2; i<=n; ++i) {
-        cnt += i;
-    }
+    // 총 숫자의 개수는 1부터 n까지의 합
+    int cnt = n * (n + 1) / 2;
 
-    int x = 0;
-    int y = 0; 
-    int dr = 0;
+    int x = 0, y = 0;   // 좌표
+    int dr = 0;         // 방향
 
     for (int i=1; i<=cnt; ++i) {
+
+        // 아래 방향
         if (dr == 0) {
             arr[x++][y] = i;
             if (x == n || arr[x][y] != 0) {
                 x--; y++;
                 dr = 1;
             }
+
+        // 오른쪽 방향
         } else if (dr == 1) {
             arr[x][y++] = i;
             if (y == n || arr[x][y] != 0) {
@@ -33,6 +35,7 @@ vector<int> solution(int n) {
                 dr = 2;
             }
             
+        // 위 방향 (대각선)
         } else {
             arr[x--][y--] = i;
             if (arr[x][y] != 0) {
@@ -42,6 +45,7 @@ vector<int> solution(int n) {
         }
     }
 
+    // 일차원 배열로 변경
     for (int i=0; i<1000; ++i) {
         for (int j=0; j<1000; ++j) {
             if (arr[i][j] == 0) break;
--- 블로그에 올림/Lv2/주식가격.cpp
+++ 블로그에 올림/Lv2/주식가격.cpp
@@ -11,30 +11,36 @@ vector<int> solution(vector<int> prices) {
     vector<int> answer;
 
     vector<int> v;
-    vector<int> idx;
     vector<int> times;
     
     v.push_back(prices[0]);
     times.push_back(0);
-    idx.push_back(0);
+
     int i=1;
     while (i < prices.size() - 1) {
+
+        // 가격이 떨어졌는지 확인
         if (v.back() <= prices[i]) {
             v.push_back(prices[i]);
-            idx.push_back(i);
             times.push_back(i);
             i++;
+
+        // 떨어짐
         } else {
-            arr[idx.back()] = i - times.back();
+            arr[times.back()] = i - times.back();
             v.pop_back();
-            idx.pop_back();
             times.pop_back();
         }
     }
+
     int k=0;
     for (int j=0; j<prices.size()-1; ++j) {
+
+        // 0: 끝까지 떨어지지 않음
         if (arr[j] == 0) {
+            // 전체에서 k초만큼 빼준다
             answer.push_back(i - times[k++]);
+
         } else {    
             answer.push_back(arr[j]);
         }
