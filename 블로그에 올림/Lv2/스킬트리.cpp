6fd3482b92403f9a647eca2add3cec51f082e774
Hertz <kljj0650@naver.com>
2022-03-01 16:37:46 +0900

    주석문 작성 및 폴더 이동

--- 블로그에 올림/Lv2/스킬트리.cpp
+++ 블로그에 올림/Lv2/스킬트리.cpp
@@ -10,19 +10,27 @@ int solution(string skill, vector<string> skill_trees) {
 
     int answer = 0;
 
+    // 순서가 존재하는 스킬과 해당 순서 표시
     for (int i=0; i<skill.size(); ++i) {
         skills[skill[i] - 'A'] = i+1;
     }
 
+    // 스킬트리 한 개씩 확인
     for (int i=0; i<skill_trees.size(); ++i) {
+
         string s = skill_trees[i];
         int level = 0;
         bool check = true;
+
         for (int j=0; j<s.size(); ++j) {
+            
+            // 배울 수 없는 스킬인지 확인
             if (skills[s[j] - 'A'] - level > 1) {
                 check = false;
                 break;
             }
+
+            // 순서가 존재하는 스킬의 현재 배운 단계 업데이트
             level = max(level, skills[s[j] - 'A']);
         }
         if (check) {
