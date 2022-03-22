63da9511bb89e261153d7e6a80162c35ec6a91cf
Hertz <kljj0650@naver.com>
2022-03-22 17:34:23 +0900

    주석문 작성 및 폴더 이동

--- 블로그에 올림/Lv2/올바른 괄호.cpp
+++ 블로그에 올림/Lv2/올바른 괄호.cpp
@@ -8,6 +8,7 @@ bool solution(string s) {
 
     bool answer = true;
 
+    // 스택
     vector<char> v;
 
     for (int i=0; i<s.size(); ++i) {
@@ -16,12 +17,14 @@ bool solution(string s) {
 
         if (c == '(') v.push_back(c);
         else {
+            // 비어있는데 ')'가 들어있으면 잘못된 괄호
             if (v.size() == 0) return false;
             if (v.back() == '(') v.pop_back();
             else return false;
         }
     }
 
+    // 순회가 끝나고 스택이 비어있으면 올바른 괄호
     if (v.size() == 0) return true;
     else return false;
 
